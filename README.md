5eea07bed693fae933dc6d8c16c0624b214b3b5e
Denys Dovhan <email@denysdovhan.com>
2017-08-31 19:08:25 +0300

    Updated at Thu, 31 Aug 2017, 19:07:49, EEST

---
+++ README.md
@@ -0,0 +1,13 @@
+# slides-vr-crash-course
+
+> Slides for my talk about VR
+
+## Scripts
+
+* `npm start` — start up the [local server](http://localhost:3000).
+* `npm build` — build the presentation in `dist` folder
+* `npm deploy` — build the presentation and deploy at [GitHub Pages](https://pages.github.com/)
+
+## License
+
+MIT © [Denys Dovhan](http://denysdovhan.com)
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(<App />, rootEl);
+
+registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
